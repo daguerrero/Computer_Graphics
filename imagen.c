d6012291d50678f85b9954282d3334e364892e8f
daguerrero <daguerrero_51@hotmail.com>
2014-08-24 14:23:26 -0500

    Arreglados los problemas con la barra negra del fondo y la creacion de la imagen

--- imagen.c
+++ imagen.c
@@ -1,4 +1,8 @@
-///Programa que crea una imagen FullHD en formato PPM
+///Programa que crea una imagen FullHD en formato PPM con colores aleatorios
+///Guerrero Romero Daniel Alberto
+///Escuela Superior de Computo
+
+///Para abrir la imagen en formato ppm utilizar eog (en gnome o derivados) o kuickshow en KDE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,17 +13,17 @@ int main()
 
 	FILE *imagen;
 	imagen = fopen("FullHD.ppm", "w");
-	fprintf(imagen, "P3,\n1920 1080\n 255\n");
+	fprintf(imagen, "P3,\n1920 1080\n255\n");
 
 	for (row=0; row<1920; row++)
 	{
-		for (col=0; col<1080; c++)
+		for (col=0; col<1080; col++)
 		{
 			red = rand() % 256;
 			green = rand() % 256;
 			blue = rand() % 256;
 
-			fprintf(imagen, "%d %d %d", red, green, blue);
+			fprintf(imagen, "%d %d %d ", red, green, blue);
 		}
 
 		fprintf(imagen, "\n");
