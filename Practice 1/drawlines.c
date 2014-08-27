6b3cf9da22033071423898a543565468172c6ccf
daguerrero <daguerrero_51@hotmail.com>
2014-08-27 17:34:12 -0500

    Practica 1
    Implementacion de la deteccion de FLIP, SWAP o FLIP & SWAP.

---
+++ Practice 1/drawlines.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+float X1=0, X2=5, Y1=1, Y2=4, m, swap, b;
+
+float Ys[1000];
+
+/*RECTA DE EJEMPLo*/
+
+
+int main(){
+
+	m = (Y2 - Y1) / (X2 - X1);
+
+	printf("Slope %f\n",m);
+
+	if(m>1){
+		/*SWAP*/
+		printf("SWAP\n");
+		swap = X1;
+		X1 = Y1;
+		Y1 = swap;
+
+		swap = X2;
+		X2 = Y2;
+		Y2 = swap;
+
+	}
+	else if(m<0 && m>-1){
+		/*FLIP*/
+		printf("FLIP\n");
+		X1 = X1*-1;
+		X2 = X2*-1;
+	}
+
+	else if(m>0 && m<1){
+		/*NADA*/
+		printf("NADA\n");
+	}
+	else if(m<-1){
+		/*SWAP Y FLIP*/
+		printf("SWAP Y FLIP\n");
+		swap = X1;
+		X1 = Y1;
+		Y1 = swap;
+
+		swap = X2;
+		X2 = Y2;
+		Y2 = swap;
+		/*FLIP*/
+		X1 = X1*-1;
+		X2 = X2*-1;
+	}
+
+	m = (Y2 - Y1) / (X2 - X1);
+	printf("NUEVA SLOPE %f\n", m);
+
+
+	/*NAIVE APROACH*/
+
+	printf("NAIVE APROACH\n");
+
+	Ys[0]=Y1;
+
+	b = (Y1 - (m*X1));
+
+	int n=1;
+
+	printf("B: %f\n", b);
+	for (X1; X1 < X2; X1++){
+		Ys[n] =	(m*X1) + (b);
+		printf("valores de Y%d: %f\n", n, Ys[n]);
+		n++; 		
+	}
+
+
+
+	return 0;
+}
