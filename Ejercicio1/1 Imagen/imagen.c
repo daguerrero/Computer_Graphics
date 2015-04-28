90a3f3f6ba156b8df70e7a41fa0cd992139ec3ce
daguerrero <daguerrero@users.noreply.github.com>
2015-04-28 12:50:25 -0500

    Programa Completo
    Solucionado el problema con el espacio que producia una barra negra al final

--- Ejercicio1/1 Imagen/imagen.c
+++ Ejercicio1/1 Imagen/imagen.c
@@ -1,34 +1,26 @@
 ///Programa que crea una imagen FullHD en formato PPM con colores aleatorios
 ///Guerrero Romero Daniel Alberto
 ///Escuela Superior de Computo
-
 ///Para abrir la imagen en formato ppm utilizar eog (en gnome o derivados) o kuickshow en KDE
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+int main(){
+
 	int col, row, red, green, blue;
-
 	FILE *imagen;
-	imagen = fopen("FullHD.ppm", "w");
-	fprintf(imagen, "P3,\n1920 1080\n255\n");
+	imagen = fopen("ImagenFULLHD.ppm", "w");
+	fprintf(imagen, "P3\n1920 1080\n255\n");
 
-	for (row=0; row<1920; row++)
-	{
-		for (col=0; col<1080; col++)
-		{
+	for (row=0; row<1920; row++){
+		for (col=0; col<1080; col++){
 			red = rand() % 256;
 			green = rand() % 256;
 			blue = rand() % 256;
 
 			fprintf(imagen, "%d %d %d ", red, green, blue);
 		}
-
 		fprintf(imagen, "\n");
 	}
-
-
 	return 0;
 }
